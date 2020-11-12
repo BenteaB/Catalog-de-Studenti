b1ec0541fca770a2f68c9ddca1f9363476b9d2f4
BenteaB <bogdanbentea@yahoo.com>
2020-11-12 14:52:35 +0200

    am adaugat comentarii

--- domain/entities.py
+++ domain/entities.py
@@ -1,6 +1,11 @@
 class Student:
 
     def __init__(self, idStudent, nume):
+        """
+        Creeaza un student
+        idStudent - numar
+        nume - string
+        """
         self.__idStudent = idStudent
         self.__nume = nume
 
@@ -14,14 +19,25 @@ class Student:
         self.__nume = value
     
     def __str__(self):
+        """
+        Returneaza un string ce contine toate detaliile unui student
+        """
         return str(self.__idStudent)+' '+self.__nume
 
     def __eq__(self,other):
+        """
+        Verifica daca doi studenti sunt egale (au acelasi id)
+        """
         return self.__idStudent == other.__idStudent
 
 class Disciplina:
 
     def __init__(self,idDisciplina,nume,profesor):
+        """
+        Creeaza o disciplina
+        idDisciplina - numar
+        nume,profesor - string
+        """
         self.__idDisciplina = idDisciplina
         self.__nume = nume
         self.__profesor = profesor
@@ -42,8 +58,14 @@ class Disciplina:
         self.__profesor = value
 
     def __str__(self):
+        """
+        Returneaza un string ce contine toate detaliile unei discipline
+        """
         return str(self.__idDisciplina)+' '+self.__nume+' '+self.__profesor
     
     def __eq__(self,other):
+        """
+        Verifica daca doua discipline sunt egale (au acelasi id)
+        """
         return self.__idDisciplina == other.__idDisciplina
 
